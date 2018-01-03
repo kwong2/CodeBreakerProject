70954920752bee43fcc0f1a80f943e5886bbe753
kwong2 <k.wong4@yahoo.com>
2018-01-03 16:54:30 -0500

    remove arguemnt and add 0 if less than 4 characters

--- src/assets/main.js
+++ src/assets/main.js
@@ -6,9 +6,11 @@ function guess() {
     //add functionality to guess function here
 }
 
-function setHiddenFields(answer) {
-   var answer = Math.floor(Math.random() * 10000);
-    
+function setHiddenFields() {
+    answer.value = Math.floor(Math.random() * 10000).toString();
+    while(answer.value.length < 4) {
+        answer.value = "0" + answer.value;
+    }   
 }
 
 //implement new functions here
